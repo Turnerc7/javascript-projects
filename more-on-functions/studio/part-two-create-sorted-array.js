0e96999146b761de25124f40d083b2732c5ce54b
Turnerc7 <turnerc777@gmail.com>
2024-07-01 21:38:13 -0500

    part one of more on function studio

--- more-on-functions/studio/part-two-create-sorted-array.js
+++ more-on-functions/studio/part-two-create-sorted-array.js
@@ -19,7 +19,15 @@ function findMinValue(arr){
 6) Be sure to print the results in order to verify your code.*/
 
 //Your function here...
+let finalSort = [];
+function numSorter(arr) {
+  while (arr.length > 0) {
+    finalSort.push(findMinValue(arr));
 
+  arr.splice(arr.indexOf(findMinValue(arr)),1);
+  }
+  return finalSort;
+  }
 /* BONUS MISSION: Refactor your sorting function to use recursion below:
  */
 
@@ -27,3 +35,4 @@ function findMinValue(arr){
 let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+console.log(numSorter(nums3));
