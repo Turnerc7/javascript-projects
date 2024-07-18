ba8863d29348bb0b08a9adc5e08aefe349c48b22
Turnerc7 <turnerc777@gmail.com>
2024-07-17 22:46:19 -0500

    Exceptions divide.js

--- exceptions/exercises/divide.js
+++ exceptions/exercises/divide.js
@@ -1,7 +1,21 @@
 // Write a function called 'divide' that takes two parameters: a numerator and a denominator.
 
+
+function divide(numerator, denominator) {
+    if (denominator === 0) {
+        throw Error('Attempted to divide by zero.');
+    }
+    return numerator / denominator;
+}
+let numerator = 6;
+let denominator = 0;
 // Your function should return the result of numerator / denominator.
 
 // However, if the denominator is zero you should throw the error, "Attempted to divide by zero."
 
 // Code your divide function here:
+try {console.log(divide(numerator, denominator));
+
+} catch (error) {
+    console.log(error.message);
+}
